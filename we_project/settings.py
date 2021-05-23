35fa701ddf2c8bfba0f9df5966ab77401c8f87e2
Semol <semolmahamud2233@gmail.com>
2021-05-23 19:25:05 +0600

    gcyhg

--- we_project/settings.py
+++ we_project/settings.py
@@ -75,24 +75,13 @@ WSGI_APPLICATION = 'we_project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/2.2/ref/settings/#datab
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'myprojectuser',
-        'PASSWORD': 'password',
-        'HOST': '192.168.0.189',
-        'PORT': '5432',
-    }
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
